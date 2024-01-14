865c108084c88da69e2f3cb761c411131597fdfc
Shankarmorti <jb009500@gmail.com>
2024-01-14 20:56:44 +0530

    initial commit

---
+++ app/src/main/java/com/example/practical42/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.practical42;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText ede = (EditText)findViewById(R.id.ede);
+        final EditText ede1 =(EditText)findViewById(R.id.ede1);
+        Button btnadd = (Button)findViewById(R.id.btnn);
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"name="+ede.getText().toString()+" " +
+                        "   marks="+ede1.getText().toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
